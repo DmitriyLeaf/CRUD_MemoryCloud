b05c4856d935425793b79044171131f56bfe4d29
Dmitry Kruhlov <dmitriy.kruglov.work@gmail.com>
2018-03-16 22:47:26 +0200

    fixed 404 eror

--- full-stack-app/full_stack_app/controllers/memory.py
+++ full-stack-app/full_stack_app/controllers/memory.py
@@ -7,7 +7,9 @@ from tg import expose, redirect, validate, flash, url
 
 from full_stack_app.lib.base import BaseController
 from full_stack_app.model import DBSession
-from full_stack_app import model
+from full_stack_app.model.memory import Memory
+
+__all__ = ['MemoryController']
 
 
 class MemoryController(BaseController):
@@ -15,13 +17,13 @@ class MemoryController(BaseController):
     # allow_only = predicates.not_anonymous()
     
     @expose('full_stack_app.templates.memory')
-    def memory(self):
+    def index(self):
     	m_name = [memorys.name for memorys in DBSession.query(Memory).order_by(Memory.name)]
         m_content = [memorys.content for memorys in DBSession.query(Memory).order_by(Memory.content)]
         m_data = [memorys.data for memorys in DBSession.query(Memory).order_by(Memory.data)]
-        m_user = [memorys.user for memorys in DBSession.query(Memory).order_by(Memory.user)]
+        #m_user = [memorys.user for memorys in DBSession.query(Memory).order_by(Memory.user)]
         return dict(page='memory',
         	m_name=m_name,
         	m_content=m_content,
-        	m_data=m_data,
-        	m_user=m_user)
+        	m_data=m_data)#,
+        	#m_user=m_user)
